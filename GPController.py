13eec9ca37548024dacbb3c7e5ee12ab08e995ad
Titta <titta.kemppi@gmail.com>
2023-11-01 09:39:14 +0200

    Add normalization to dataloader

--- GPController.py
+++ GPController.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import gpytorch
 import torch
+import time
 
 import data.dataloader as dataloader
 from training.ExactGP import ExactGPModel
@@ -11,35 +12,52 @@ import matplotlib.pyplot as plt
 
 train_path = "data/training1.csv"
 test_path = "data/testing1.csv"
-training_iter = 1
+training_iter = 20 
 num_tasks = 5
 
 def main():
     #Load data
-    X_train, y_train = dataloader.load_training_data(train_path=train_path, normalize=False)
-    X_test, y_test = dataloader.load_test_data(test_path=test_path, normalize=False)
+
+    X_train, y_train = dataloader.load_training_data(train_path=train_path, normalize=True)
+    X_test, y_test = dataloader.load_test_data(test_path=test_path, normalize=True)
 
     likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks)
     model = BatchIndependentMultitaskGPModel(X_train, y_train, likelihood, num_tasks)
 
+    # Check if a GPU is available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+
+    # Move data tensors to the GPU
+    X_train, y_train = X_train.to(device), y_train.to(device)
+    X_test = X_test.to(device)
+
+    start_model_training = time.perf_counter()
     #Find optimal model hyperparameters
     model.train()
     likelihood.train()
 
     #Adam
     optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    batch_size = 32  # Choose a smaller batch size
 
     #Loss
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-
+    scaler = torch.cuda.amp.grad_scaler
     for i in range(training_iter):
         optimizer.zero_grad()
-        output = model(X_train)
-        loss = -mll(output, y_train)
-        loss.backward()
+        with torch.cuda.amp.autocast_mode.autocast():
+            output = model(X_train)
+            loss = -mll(output, y_train)
+        scaler.GradScaler.scale(loss).backward()
+        scaler.GradScaler.step(optimizer)
+        scaler.GradScaler.update()
         print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iter, loss.item()))
-        optimizer.step()
-    
+        
+    end_model_training = time.perf_counter()
+    elapsed_model_training = end_model_training - start_model_training
+    print(elapsed_model_training)
+
     model.eval()
     likelihood.eval()
 
@@ -48,27 +66,26 @@ def main():
 
     # Define the tasks
     tasks = ["theta1", "theta2", "xt2", "x_boom", "y_boom"]
-            
-    # Make predictions for each task
-    with torch.no_grad(), gpytorch.settings.fast_pred_var():
-        test_x = torch.linspace(0, 1, 10058)
-        predictions = likelihood(model(X_test))
-        mean = predictions.mean
-        print(mean)
-        lower, upper = predictions.confidence_region()
-    print(test_x.shape)
-    print(y_train[0,:].shape)
-    #Plot training data as black stars
-    axes[0].plot(test_x.numpy(), y_test[0,:].numpy(), 'k*')
-    axes[0].plot(test_x.numpy(), mean[:,0].numpy(), 'b')
-    # Shade in confidence
-    axes[0].fill_between(test_x.numpy(), lower[:, 0].numpy(), upper[:, 0].numpy(), alpha=0.5)
-    axes[0].set_ylim([-3, 3])
-    axes[0].legend(['Observed Data', 'Mean', 'Confidence'])
-    axes[0].set_title('Observed Values (Likelihood)')
-    fig.show()
+    
+    for i, task in enumerate(tasks):
+        # Make predictions for each task
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            test_x = torch.linspace(0, 1, 10058)
+            predictions = likelihood(model(X_test))
+            mean = predictions.mean
+            lower, upper = predictions.confidence_region()
 
+        #Plot training data as black stars
+        axes[i].plot(test_x.numpy(), y_train[i,:].numpy(), 'k*')
+        axes[i].plot(test_x.numpy(), mean[:,i].numpy(), 'b')
+        # Shade in confidence
+        axes[i].fill_between(test_x.numpy(), lower[:, i].numpy(), upper[:, i].numpy(), alpha=0.5)
+        axes[i].set_ylim([-1, 1])
+        axes[i].legend(['Observed Data', 'Mean', 'Confidence'])
+        axes[i].set_title('Observed Values (Likelihood)')
+        
+    plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main() 
--- data/dataloader.py
+++ data/dataloader.py
@@ -2,46 +2,61 @@ import numpy as np
 import pandas as pd
 import torch
 
-def normalize_data(data):
-    """"TODO: Normalize data with mean and std"""
-    return
+def normalize_data(X1, y1, mean, std, epsilon=1e-8):
+    X = np.zeros(X1.shape)
+    X[:,0:5] = np.divide(X1[:, :-3] - mean, std+epsilon)
+    X[:,5:] = X1[:, 5:]
+    y = np.divide(y1, std+epsilon)
+    
+    X = torch.tensor(X, dtype=torch.float)
+    y = torch.tensor(y, dtype=torch.float)
+    return X, y
+
+def calculate_mean_std(X):
+    mean = np.mean(X[:, :5], axis=0)
+    std = np.std(X[:, :5], axis=0)
+    return mean, std
 
 def load_data(path):
     return pd.read_csv(path)
 
 def get_xy(data):
     try:
-        
-        #Time?
-        X = data[["theta1", "theta2", "xt2", "fc1", "fc2", "fct2", "x_boom", "y_boom"]] #States == Joint values + Control input == Torques
-        #Output => State 1 state after, add torques if need be
+        X = data[["theta1", "theta2", "xt2", "x_boom", "y_boom", "fc1", "fc2", "fct2"]]
         y = data[["theta1", "theta2", "xt2", "x_boom", "y_boom"]]
-        print(X.shape)
-        print(y.shape)
-        #Output is the difference of expected and current state
+
         y = y - y.shift(1) 
         y.iloc[0] = y.iloc[1]
-        #Convert to tensors
-        X = torch.tensor(X.values, dtype=torch.float)
-        y = torch.t(torch.tensor(y.values, dtype=torch.float))
         return X, y
     except:
         raise AttributeError("Invalid data format")
 
-def load_training_data(train_path, normalize=False):
+def load_training_data(train_path, normalize=True):
     train_data = load_data(train_path)
-    X, y  = get_xy(train_data)  
+    X, y = get_xy(train_data)  
     
-    #Add normalization
+    mean_states, std_states = calculate_mean_std(X.values)
+    if normalize:
+        X, y = normalize_data(X.values, y.values, mean_states, std_states)
+    else:
+        X = torch.tensor(X, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.float)
 
-    return X,y 
+    return X, y 
 
 def load_test_data(test_path, normalize=False):
     test_data = load_data(test_path)
     X, y = get_xy(test_data)
-    return X,y
 
-if __name__=="__main__":
+    mean_states, std_states = calculate_mean_std(X.values)
+    if normalize:
+        X, y = normalize_data(X.values, y.values, mean_states, std_states)
+    else:
+        X = torch.tensor(X, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.float)
+    return X, y
+
+if __name__ == "__main__":
     train_path = "data/training1.csv"
     test_path = "data/testing1.csv"
     
@@ -49,7 +64,5 @@ if __name__=="__main__":
 
     X_test, y_test = load_test_data(test_path, True)
 
-    #print(X_train)
-    #print(y_train)
+    print(X_train)
     print(y_train)
-    print(y_test)
