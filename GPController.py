Add data with simple trajectory and larger step size. Fix dataloader normalization=False option

GPController.py
@@ -13,9 +13,9 @@ from torch.utils.data import DataLoader, TensorDataset
 
 torch.cuda.memory_summary(device=None, abbreviated=False)
 
-train_path = "data/training2_short.csv"
-test_path = "data/testing2_short.csv"
-training_iter = 10
+train_path = "data/training1_simple_10Hz.csv"
+test_path = "data/testing1_simple_10Hz.csv"
+training_iter = 100
 num_tasks = 1
 batch_size = 16  # Updated batch size
 
@@ -42,7 +42,7 @@ def main():
     likelihood.train()
 
     # Adam optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
     # Loss
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
@@ -84,7 +84,7 @@ def main():
         plt.plot(test_x.cpu().numpy(), y_test[:, i].cpu().numpy(), 'k*')
         plt.plot(test_x.cpu().numpy(), mean[:, i].cpu().numpy(), 'b')
         # Shade in confidence
-        #plt.fill_between(test_x.cpu().numpy(), lower[:, i].cpu().numpy(), upper[:, i].cpu().numpy(), alpha=0.5)
+        plt.fill_between(test_x.cpu().numpy(), lower[:, i].cpu().numpy(), upper[:, i].cpu().numpy(), alpha=0.5)
         #plt.ylim(-3, 3)
         plt.legend(['Observed Data', 'Mean', 'Confidence'])
         plt.title(f'Observed Values (Likelihood) for Task {task}')

data/dataloader.py
@@ -6,10 +6,10 @@ from sklearn.preprocessing import StandardScaler
 
 def normalize_data(X1, y1, mean, std, epsilon=1e-8):
     X = np.zeros(X1.shape)
-    #X[:,0:1] = np.divide(X1[:, 1:] - mean, std+epsilon)
-    #X[:,1:] = X1[:, 1:]
+    X[:,0:1] = np.divide(X1[:, 1:] - mean, std+epsilon)
+    X[:,1:] = X1[:, 1:]
 
-    X = np.divide(X1 -mean, std+epsilon)
+    #X = np.divide(X1 -mean, std+epsilon)
     y = np.divide(y1, std+epsilon)
     
     scaler = StandardScaler()
@@ -53,14 +53,13 @@ def get_xy(data):
 def load_training_data(train_path, normalize=True):
     train_data = load_data(train_path)
     X, y = get_xy(train_data)  
-    print(X)
-    print(y)
+
     mean_states, std_states = calculate_mean_std(X.values)
     if normalize:
         X, y = normalize_data(X.values, y.values, mean_states, std_states)
     else:
-        X = torch.tensor(X, dtype=torch.float)
-        y = torch.tensor(y, dtype=torch.float)
+        X = torch.tensor(X.values, dtype=torch.float)
+        y = torch.tensor(y.values, dtype=torch.float)
 
     return X, y 
 
@@ -72,8 +71,8 @@ def load_test_data(test_path, normalize=False):
     if normalize:
         X, y = normalize_data(X.values, y.values, mean_states, std_states)
     else:
-        X = torch.tensor(X, dtype=torch.float)
-        y = torch.tensor(y, dtype=torch.float)
+        X = torch.tensor(X.values, dtype=torch.float)
+        y = torch.tensor(y.values, dtype=torch.float)
     return X, y
 
 def plot_X_train_vs_time(X_train):
@@ -95,8 +94,8 @@ def plot_X_train_vs_time(X_train):
         plt.show()
 
 if __name__ == "__main__":
-    train_path = "data/training2_short.csv"
-    test_path = "data/testing2_short.csv"
+    train_path = "data/training1_simple_10Hz.csv"
+    test_path = "data/testing1_simple_10Hz.csv"
     
     X_train, y_train = load_training_data(train_path, True)
 
@@ -104,6 +103,7 @@ if __name__ == "__main__":
 
     print(X_train)
     print(y_train)
+    plot_X_train_vs_time(X_train)
     plot_X_train_vs_time(X_test)
 
 