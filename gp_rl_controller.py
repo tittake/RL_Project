f682ffdbfda4af5adffa20d37e448e222289d485
Titta <titta.kemppi@gmail.com>
2023-11-17 10:39:53 +0200

    Model that gives good resultsgit add gp_rl_controller.py

--- gp_rl_controller.py
+++ gp_rl_controller.py
@@ -1,8 +1,8 @@
 from GPController import GPModel
 from RL.controller import RLController, PolicyNetwork
 
-train_path = "data/training1_simple_100Hz.csv"
-test_path = "data/testing1_simple_100Hz.csv"
+train_path = "data/training1_simple_10Hz.csv"
+test_path = "data/testing1_simple_10Hz.csv"
 training_iter = 100
 num_tasks = 1
 batch_size = 16  # Updated batch size
