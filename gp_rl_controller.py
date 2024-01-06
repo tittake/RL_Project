80c3256cda829533b9c6ee2c385b8a467fa6cb87
Casey Jones <casey.jones@caci.com>
2024-01-06 02:03:06 +0200

    replace argparse with yaml configuration file

--- gp_rl_controller.py
+++ gp_rl_controller.py
@@ -1,6 +1,6 @@
-import argparse
 import numpy as np
 import torch
+import yaml
 
 from training.GPController import GPModel
 from RL.controller import RLController
@@ -15,32 +15,27 @@ def get_configs():
     return
 
 """Collective controller for GP model and RL controller"""
-def main(opts):
+def main(configuration):
 
     #Initialize and train model or load pre-trained model
-    gpmodel = GPModel(**vars(opts))
+    gpmodel = GPModel(**configuration)
 
     gpmodel.plot_training_results()
         
-    opts.gp_model = gpmodel
-    print(gpmodel.predict([0,0,0,1,1,1]))
+    configuration.gp_model = gpmodel
 
-    policy_network = PolicyNetwork(**vars(opts))
+    print(gpmodel.predict([0, 0, 0, 1, 1, 1]))
+
+    policy_network = PolicyNetwork(**configuration)
 
     #policy_network.optimize_policy()
 
 #TODO: Separate GP and RL configs
 if __name__ == "__main__":
-    opts = argparse.Namespace()
-    #opts.train_path = "data/some_chill_trajectories/trajectory14_100Hz.csv"
-    #opts.test_path = "data/some_chill_trajectories/trajectory46_100Hz.csv"
-    opts.train_path = "data/two-joint_trajectories_10Hz/trajectory2.csv" #Only use one trajectory test and train
-    opts.test_path = "data/two-joint_trajectories_10Hz/trajectory6.csv" 
-    opts.data_directory = 'data/two-joint_trajectories_10Hz' #Use a whole directory of data
-    opts.num_tasks = 2
-    opts.ard_num_dims = 6
-    opts.training_iter = 150
-    opts.train_GP = True
-    opts.model_path = 'trained_models/two_joints_GP.pth'
 
-    main(opts)
+    with open("configuration.yaml") as configuration_file:
+
+        configuration = yaml.load(configuration_file,
+                                  Loader = yaml.Loader)
+    
+    main(configuration)
