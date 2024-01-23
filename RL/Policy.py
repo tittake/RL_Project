ff20d57b53e2ff534c9c621d815f409e334f5236
Casey Jones <casey.jones@tuni.fi>
2024-01-23 18:18:23 +0200

    RL: first steps towards training with batches

--- RL/Policy.py
+++ RL/Policy.py
@@ -1,9 +1,12 @@
 """module containing reinforcement learning policy class"""
 
+import csv
 from copy import deepcopy
 import json
 from math import sqrt
-from random import random
+from os import listdir
+from os.path import join
+from random import randint, random
 import time
 import torch
 from torch import cdist, unsqueeze
@@ -11,7 +14,6 @@ import numpy as np
 
 import data.dataloader as dataloader
 from RL.Controller import RlController
-from RL.utils import get_random_state
 
 from GP.GpModel import GpModel
 import matplotlib.pyplot as plt
@@ -38,10 +40,7 @@ class PolicyNetwork:
         self.learning_rate = learning_rate
 
         # TODO make this configurable from someplace
-        self.controller_input_features = ("target",
-                                          "joints",
-                                          "velocities",
-                                          "accelerations")
+        # TODO compute state_feature_count from this (how to know target dim?)
 
         self.scalers = self.gp_model.scalers
 
@@ -57,8 +56,240 @@ class PolicyNetwork:
 
         self.rewards = []
 
+    def get_random_states(self, batch_size):
+        """
+        fetches a batch of random states & next states from the trajectories
+        in self.data_path
+
+        A random state, including a random target, is fetched from each
+        trajectory until the specified batch size is reached. If the number of
+        trajectories is smaller than the desired batch size, the trajectories
+        will be iterated further until the desired batch size is reached. The
+        random state is taken from somewhere in the beginning 80% of each
+        trajectory's data. The target is taken from somewhere between 20% after
+        the initial state and the end of the trajectory.
+
+        returns tuple of dicts (states, next_states)
+
+        Each dict in the returned tuple comprises:
+            keys: high-level features
+            values: tensors of size (batch_size, number of low-level features)
+
+        A high level feature is for example "velocities", which might contain
+        two lower-level features for the x- and y-components respectively.
+        Therefore, for a batch size of 100, states["velocities].shape and
+        next_states["velocities"] would both be (100, 2).
+        """
+
+        # TODO return not only states, but their NEXT states, to calculate rewards!
+
+        csv_files = [file for file
+                     in listdir(self.data_path)
+                     if file.endswith('.csv')]
+
+        list_of_states      = []
+
+        while len(list_of_states) < batch_size:
+
+            for csv_file in csv_files:
+
+                file_path = join(self.data_path, csv_file)
+
+                with open(file_path, "rb") as trajectory_file:
+                    lines_in_file = sum(1 for _ in trajectory_file)
+
+                last_line_in_file = lines_in_file - 2
+
+                state_index = randint(a = 1,
+                                      b = round(0.8 * last_line_in_file))
+
+                next_state_index = state_index + 1
+
+                target_index = randint(a =  (state_index
+                                             + round(0.2 * last_line_in_file)),
+                                       b = last_line_in_file)
+
+                state = {}
+
+                next_state = {}
+
+                with open(file_path, 'r') as trajectory_file:
+
+                    csv_reader = csv.DictReader(trajectory_file)
+
+                    for index, row in enumerate(csv_reader):
+
+                        if index in (state_index, next_state_index):
+
+                            the_state = (state
+                                         if index == state_index
+                                         else next_state)
+
+                            for feature in dataloader.y_features:
+
+                                the_state[feature] = \
+                                          [float(row[feature])
+                                           for feature
+                                           in dataloader.features[feature]]
+
+                        if index == target_index:
+
+                            for the_state in (state, next_state):
+
+                                the_state["target"] = \
+                                    [float(row[feature])
+                                     for feature
+                                     in dataloader.features["ee_location"]]
+
+                            break # quit looping this particular CSV file
+
+                    assert "target" in state
+
+                    list_of_states.append((state, next_state))
+
+                if len(list_of_states) == batch_size:
+                    break # quit looping over CSV files
+
+        self.scalers["target"] = self.scalers["ee_location"]
+
+        scaled_states      = {}
+        scaled_next_states = {}
+
+        for index, the_scaled_states in enumerate((scaled_states,
+                                                   scaled_next_states)):
+
+            for feature in ["target", *dataloader.y_features]:
+
+                feature_data = \
+                    np.array([state[index][feature]
+                              for state in list_of_states])
+
+                scaled_feature_data = \
+                    self.scalers[feature].transform(feature_data)
+
+                the_scaled_states[feature] = \
+                    torch.tensor(data   = scaled_feature_data,
+                                 device = self.device,
+                                 dtype  = self.dtype)
+
+        return scaled_states, scaled_next_states
+
+    def calculate_rewards(self, states):
+        """calculate and return reward for a single time step"""
+
+        # TODO should this be a static method?
+        # TODO update docstring
+
+        # vector from the predicted EE coordinates towards the goal
+        ideal_vector_to_goal = (  states["target"]
+                                - states["ee_location"])
+
+        # TODO calculate the dot product between this and acceleration
+
+        error_metrics = {}
+
+        for vector_metric in ("accelerations", "velocities"):
+
+            vectors = (self.scalers[vector_metric]
+                       .inverse_transform(states[vector_metric]))
+
+            # FIXME but i would lose my gradients here with the inverse transformation... :(
+
+            vectors = \
+                torch.tensor(data   = vectors,
+                             device = self.device,
+                             dtype  = self.dtype)
+
+            # print(ideal_vector_to_goal.shape)
+            # print(vectors.shape)
+            # print(vectors.T.shape)
+
+            # https://stackoverflow.com/a/65331075/837710
+
+            batch_size    = vectors.shape[0]
+            feature_count = vectors.shape[1]
+
+            print(ideal_vector_to_goal[-5:])
+
+            print(vectors[-5:])
+
+            x = ideal_vector_to_goal.reshape(batch_size, 1, feature_count)
+            y = vectors.reshape(batch_size, feature_count, 1)
+
+            dot_product = torch.matmul(x, y).squeeze((1, 2))
+
+            # print(ideal_vector_to_goal[-4:])
+            # print(vectors[-4:])
+            print(dot_product[-5:])
+            # print(dot_product.shape)
+
+            norm = (  torch.norm(ideal_vector_to_goal,  dim=1)
+                    * torch.norm(vectors, dim=1))
+
+            print(norm[-5:])
+            # print(norm.shape)
+            # exit()
+
+            print((dot_product / norm)[-5:])
+
+            # calculate the angle between the vectors
+            error_metrics[vector_metric] = \
+                torch.arccos(torch.div(dot_product, norm))
+
+            # FIXME why is this very often greater than 1/2 * pi? e.g. 2.0476, 2.6569...
+
+            print(error_metrics[vector_metric][-5:])
+            # print(error_metrics[vector_metric].shape)
+
+            # normalize to range (0, 1)
+            error_metrics[vector_metric] = \
+                torch.div(error_metrics[vector_metric],
+                          ((1 / 2) * torch.pi))
+
+            print(error_metrics[vector_metric][-5:])
+            # print(error_metrics[vector_metric].shape)
+
+            # FIXME why are the errors usually so high?
+            # [0.7897, 0.9199, 1.0976, 0.8541, 0.3119]
+            # these are ground-truth trajectories which should be headed in the right direction...
+            # oh SHIT!
+            # i'm dealing with values SCALED by different scalers...
+            # but i just scaled them, in self.get_random_states()...
+            # do i NOT want to do that?
+            # i guess that i probably DO, since they will often come from GP
+
+            exit()
+
+        error_metrics["euclidian_distance"] = \
+            cdist(unsqueeze(states["ee_location"], dim=0),
+                  unsqueeze(states["target"],      dim=0))
+
+        # normalize Euclidian distance to range (0, 1)
+        # x and y coordinates should be between -1 and 1
+        # thus maximum possible distance is [-1, -1] → [1, 1] = 2 * sqrt(2)
+        error_metrics["euclidian_distance"] = \
+            torch.div(error_metrics["euclidian_distance"],
+                      (2 * sqrt(2)))
+
+        reward = -sum((1 / len(error_metrics)) * error
+                      for error in error_metrics.values())
+
+        print(f"reward: {reward.item()}")
+
+        exit()
+
+        return reward
+
     def optimize_policy(self):
         """optimize controller parameters"""
+
+        states, next_states = self.get_random_states(batch_size = 100)
+
+        rewards = self.calculate_rewards(states)
+
+        # TODO calculate rewards
+
+        return
 
         trials = self.trials
 
@@ -147,8 +378,12 @@ class PolicyNetwork:
         elapsed_model_training = end_model_training - start_model_training
         print("training time: ", elapsed_model_training)
 
-    def calculate_step_reward(self):
-        """calculate and return reward for a single time step"""
+    def select_actions(self):
+
+        # TODO arguments: states
+        # TODO returns actions
+        # TODO handle batches
+        # TODO docstring
 
         def print_value(title, tensor):
             """print a tensor's value for debugging"""
@@ -164,7 +399,6 @@ class PolicyNetwork:
 
         exploiting = False
 
-        # TODO doesn't make sense to choose action in calculate_step_reward()
         if random() <= 0.5:
 
             print("exploiting...")
@@ -194,6 +428,13 @@ class PolicyNetwork:
 
         print()
 
+    def predict_next_states(self):
+
+        # TODO arguments: current states, actions
+        # TODO return next states, rather than setting self.state?
+        # TODO handle batches
+        # TODO docstring
+
         gp_inputs = [self.state[feature]
                      for feature in dataloader.X_features]
 
@@ -212,96 +453,3 @@ class PolicyNetwork:
             self.state[feature] = predictions.mean[0, start_index : end_index]
 
             self.state[feature].to(self.device, dtype = self.dtype)
-
-        # vector from the predicted EE coordinates towards the goal
-        ideal_vector_to_goal = (  self.state["target"]
-                                - self.state["ee_location"])
-
-        # TODO calculate the dot product between this and acceleration
-
-        error_metrics = {}
-
-        for vector_metric in ("accelerations", "velocities"):
-
-            dot_product = torch.dot(ideal_vector_to_goal,
-                                    self.state[vector_metric])
-
-            dot_product = torch.dot(ideal_vector_to_goal,
-                                    self.state[vector_metric]
-                                    ).to(self.device, dtype = self.dtype)
-
-            norm = (  torch.norm(ideal_vector_to_goal)
-                    * torch.norm(self.state[vector_metric]))
-
-            # calculate the angle between the vectors
-            error_metrics[vector_metric] = \
-                torch.arccos(torch.div(dot_product, norm))
-
-            # normalize to range (0, 1)
-            error_metrics[vector_metric] = \
-                torch.div(error_metrics[vector_metric],
-                          ((1 / 2) * torch.pi))
-
-        error_metrics["euclidian_distance"] = \
-            cdist(unsqueeze(self.state["ee_location"], dim=0),
-                  unsqueeze(self.state["target"],      dim=0))
-
-        # normalize Euclidian distance to range (0, 1)
-        # x and y coordinates should be between -1 and 1
-        # thus maximum possible distance is [-1, -1] → [1, 1] = 2 * sqrt(2)
-        error_metrics["euclidian_distance"] = \
-            torch.div(error_metrics["euclidian_distance"],
-                      (2 * sqrt(2)))
-
-        reward = -sum((1 / len(error_metrics)) * error
-                      for error in error_metrics.values())
-
-        if exploiting:
-
-            self.rewards.append(reward.cpu().detach().item())
-
-            with open("rewards.json", "w") as rewards_log:
-                json.dump(self.rewards, rewards_log, indent = 1)
-
-        print(f"reward: {reward.item()}")
-
-        return reward
-
-    def reset(self):
-        """set initial & goal states"""
-
-        random_state = get_random_state(self.data_path)
-
-        initial_state = {}
-
-        for feature in dataloader.y_features:
-
-            initial_state[feature] = \
-                np.array([[random_state[column]
-                          for column in dataloader.features[feature]]])
-
-            initial_state[feature] = \
-                torch.tensor(self.scalers[feature]
-                             .transform(initial_state[feature]),
-                             device = self.device,
-                             dtype  = self.dtype)[0]
-
-        initial_state["target"] = initial_state["ee_location"].clone()
-
-        # loop till initial & goal locations distinct (to avoid divide by zero)
-        while torch.equal(initial_state["target"],
-                          initial_state["ee_location"]):
-
-            random_state = get_random_state(self.data_path)
-
-            initial_state["target"] = np.array([[random_state["boom_x"],
-                                                 random_state["boom_y"]]
-                                                ])
-
-            initial_state["target"]= \
-                torch.tensor(self.scalers["ee_location"]
-                             .transform(initial_state["target"]),
-                             device = self.device,
-                             dtype  = self.dtype)[0]
-
-        self.state = initial_state
