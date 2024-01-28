1259947042848a673e843ffda327879bab530c33
Casey Jones <casey.jones@tuni.fi>
2024-01-28 11:48:41 +0200

    Policy: batch select_actions, predict_next_states

--- RL/Policy.py
+++ RL/Policy.py
@@ -41,6 +41,10 @@ class PolicyNetwork:
 
         # TODO make this configurable from someplace
         # TODO compute state_feature_count from this (how to know target dim?)
+        self.controller_input_features = ("target",
+                                          "joints",
+                                          "velocities",
+                                          "accelerations")
 
         self.scalers = self.gp_model.scalers
 
@@ -271,110 +275,38 @@ class PolicyNetwork:
     def optimize_policy(self):
         """optimize controller parameters"""
 
-        states, next_states = self.get_random_states(batch_size = 100)
+        optimizer = torch.optim.Adam(self.controller.parameters(),
+                                     lr=self.learning_rate)
 
-        rewards = self.calculate_rewards(states)
+        self.controller.train()
 
-        print(rewards.shape) # [1, 100, 100]
-        # hmm, this is wrong... it should be just [100] i think...
+        for i in range(self.iterations):
 
-        # TODO calculate rewards
+            optimizer.zero_grad()
 
-        return
+            states, next_states = self.get_random_states(batch_size = 100)
 
-        trials = self.trials
+            # try calculating loss between real next_states & model predictions
 
-        for trial in range(trials):
+            actions = self.select_actions(states)
 
-            self.reset()
+            states = self.predict_next_states(states, actions)
 
-            optimizer = torch.optim.Adam(self.controller.parameters(),
-                                         lr=self.learning_rate)
+            rewards = self.calculate_rewards(states)
+            # rewards = self.calculate_rewards(next_states)
 
-            optimization_log = {"loss": [],
-                                "time": []}
+            loss = -rewards
 
-            initial_distance = \
-                cdist(unsqueeze(self.state["ee_location"], dim=0),
-                      unsqueeze(self.state["target"],      dim=0)
-                      ).item()
+            loss.backward()
 
-            print(f"initial_distance: {initial_distance}\n")
+            optimizer.step()
 
-            initial_ee_location = \
-                self.state["ee_location"].clone().cpu().detach().numpy()
+    def select_actions(self, states):
+        """
+        given a batch of states, output a batch of actions
 
-            start_model_training = time.perf_counter()
-
-            self.controller.train()
-
-            for i in range(self.iterations):
-
-                print(f"trial {trial + 1}/{self.trials}, "
-                      f"iteration {i + 1}/{self.iterations}\n")
-
-                print(f"initial EE location: {initial_ee_location}")
-
-                print("current EE location: "
-                      f"{self.state['ee_location'].cpu().detach().numpy()}")
-
-                print("target  EE location: "
-                      f"{self.state['target'].cpu().detach().numpy()}\n")
-
-                optimizer.zero_grad()
-
-                reward = self.calculate_step_reward()
-
-                loss = -reward
-
-                loss.backward()
-
-                distance = \
-                    cdist(unsqueeze(self.state["ee_location"], dim=0),
-                          unsqueeze(self.state["target"],      dim=0)
-                          ).cpu().detach().item()
-
-                print(f"distance: {distance}")
-
-                percent_distance_covered = (  (initial_distance - distance)
-                                            / initial_distance)
-
-                print("percent distance covered: "
-                      f"{percent_distance_covered:.1%}\n")
-
-                optimizer.step()
-
-                optimization_log["loss"].append(loss.item())
-
-            print(f"trial {trial + 1} distance covered: "
-                  f"{percent_distance_covered:.1%}\n")
-
-            if False:
-                _, ax_loss = plt.subplots(figsize=(6, 4))
-
-                ax_loss.plot(optimization_log["loss"], label='Training Loss')
-
-                ax_loss.set_title('Training Loss Over Iterations')
-                ax_loss.set_xlabel('Iteration')
-                ax_loss.set_ylabel('Loss')
-
-                ax_loss.legend()
-
-                plt.show()
-
-            torch.save(self.controller.state_dict(),
-                       f"trained_models/RL-{trial + 1:03}.pth")
-
-        end_model_training = time.perf_counter()
-        elapsed_model_training = end_model_training - start_model_training
-        print("training time: ", elapsed_model_training)
-
-    def select_actions(self):
-
-        # TODO arguments: states
-        # TODO returns actions
-        # TODO handle batches
-        # TODO docstring
+        currently alternates between exploiting & exploring with ε = 0.5
+        """
 
         def print_value(title, tensor):
             """print a tensor's value for debugging"""
@@ -383,10 +315,7 @@ class PolicyNetwork:
 
         # detach gradients to avoid accumulation due to looping models
         for feature in self.controller_input_features:
-            self.state[feature] = \
-                self.state[feature].to(self.device,
-                                       dtype = self.dtype
-                                       ).detach()
+            states[feature] = states[feature].detach()
 
         exploiting = False
 
@@ -396,43 +325,49 @@ class PolicyNetwork:
 
             exploiting = True
 
-            controller_inputs = [self.state[feature]
+            controller_inputs = [states[feature]
                                  for feature
                                  in self.controller_input_features]
 
-            controller_inputs = \
-                unsqueeze(torch.cat(controller_inputs), dim=0)
+            controller_inputs = torch.cat(controller_inputs, dim=1)
 
-            self.state["torques"] = self.controller(controller_inputs)[0]
+            actions = self.controller(controller_inputs)
 
         else:
 
             print("exploring...")
 
-            # random (normalized) torques between -1 and 1
-            self.state["torques"] = -1 + 2 * torch.rand(size  = (3, ),
-                                                        dtype = self.dtype
-                                                        ).to(self.device)
+            batch_size = len(states[self.controller_input_features[0]])
 
-        for feature, value in self.state.items():
-            print_value(title = feature, tensor = value)
+            # random torques normalized between -1 and 1
+            actions = -1 + 2 * torch.rand(size  = (batch_size, 3),
+                                          dtype = self.dtype
+                                          ).to(self.device)
 
-        print()
+        # for feature, value in states.items():
+            # print_value(title = feature, tensor = value)
 
-    def predict_next_states(self):
+        return actions
 
-        # TODO arguments: current states, actions
-        # TODO return next states, rather than setting self.state?
-        # TODO handle batches
-        # TODO docstring
+    def predict_next_states(self, states, actions):
+        """
+        given a batch of states and a batch of actions,
+        return the batch of next states
+        """
 
-        gp_inputs = [self.state[feature]
+        states["torques"] = actions
+
+        for feature in dataloader.X_features: # check batch size matches
+            assert states[feature].shape[0] == actions.shape[0]
+
+        gp_inputs = [states[feature]
                      for feature in dataloader.X_features]
 
-        gp_inputs = unsqueeze(torch.cat(gp_inputs), dim=0)
+        gp_inputs = torch.cat(gp_inputs, dim=1)
 
         predictions = self.gp_model.predict(gp_inputs)
 
+        # extract and update state with each high-level feature from output
         for feature in dataloader.y_features:
 
             (start_index,
@@ -441,6 +376,8 @@ class PolicyNetwork:
                     feature_names = dataloader.y_features,
                     query_feature = feature)
 
-            self.state[feature] = predictions.mean[0, start_index : end_index]
+            states[feature] = predictions.mean[0, start_index : end_index]
 
-            self.state[feature].to(self.device, dtype = self.dtype)
+            states[feature].to(self.device, dtype = self.dtype)
+
+        return states
